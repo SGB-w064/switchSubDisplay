348601d10622a1094cd29a57a6d0a83d7196ab9b
Yuya Sogabe <hw19a064@oecu.jp>
2020-12-31 17:40:40 +0900

    first commit

---
+++ SubDisplaySwitch.py
@@ -0,0 +1,15 @@
+import ctypes
+import subprocess as sp
+
+user32 = ctypes.windll.user32
+
+singleScreenSize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+allScreenSize = user32.GetSystemMetrics(78), user32.GetSystemMetrics(79)
+
+if singleScreenSize == allScreenSize:
+    cmd = ["powershell", "-noexit", "&", "DisplaySwitch.exe", "/extend"]
+else:
+    cmd = ["powershell", "-noexit", "&", "DisplaySwitch.exe", "/internal"]
+
+sp.run(cmd)
+
